ca21486e7d5636a92077b21044e2225f06869bbf
nujarum <84607911+nujarum@users.noreply.github.com>
2022-09-19 22:51:00 +0900

    test: refactoring & code housekeeping

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,26 +1,55 @@
-import { URL } from 'node:url';
-import { expect, test } from '@jest/globals';
-import { importMetaResolve, importMetaResolveAll } from '#index';
+import { execFile } from 'node:child_process';
+import { promisify } from 'node:util';
+import { beforeAll, describe, expect, test } from '@jest/globals';
 
-test('importMetaResolve', async () => {
-    expect(typeof importMetaResolve).toBe('function');
-    const actual = await importMetaResolve('tslib');
-    const expected = new URL('../node_modules/tslib/modules/index.js', import.meta.url).href;
-    expect(actual).toBe(expected);
+process.execArgv.push('--conditions=development');
+const { importMetaResolve, importMetaResolveAll } = await import('#index');
+const thisUrl = import.meta.url;
+
+describe('general use case', () => {
+
+    test('importMetaResolve', async () => {
+        expect(typeof importMetaResolve).toBe('function');
+        const actual = await importMetaResolve('tslib');
+        const expected = new URL('../node_modules/tslib/modules/index.js', thisUrl).href;
+        expect(actual).toBe(expected);
+    });
+
+    test('importMetaResolveAll', async () => {
+        expect(typeof importMetaResolveAll).toBe('function');
+        const actual = await importMetaResolveAll([
+            '@rollup/plugin-node-resolve',
+            '@rollup/pluginutils',
+            'rollup',
+        ]);
+        const expected = [
+            new URL('../node_modules/@rollup/plugin-node-resolve/dist/es/index.js', thisUrl).href,
+            new URL('../node_modules/@rollup/pluginutils/dist/es/index.js', thisUrl).href,
+            new URL('../node_modules/rollup/dist/es/rollup.js', thisUrl).href,
+        ];
+        expect(actual).toEqual(expected);
+    });
+
 });
 
-test('importMetaResolveAll', async () => {
-    expect(typeof importMetaResolveAll).toBe('function');
-    const current = import.meta.url;
-    const actual = await importMetaResolveAll([
-        '@rollup/plugin-node-resolve',
-        '@rollup/pluginutils',
-        'rollup',
-    ]);
-    const expected = [
-        new URL('../node_modules/@rollup/plugin-node-resolve/dist/es/index.js', current).href,
-        new URL('../node_modules/@rollup/pluginutils/dist/es/index.js', current).href,
-        new URL('../node_modules/rollup/dist/es/rollup.js', current).href,
-    ];
-    expect(actual).toEqual(expected);
+describe('with user conditions', () => {
+
+    const pkgDir = new URL('./dummy/pkg/', thisUrl);
+    const libDir = new URL('./dummy/lib/', thisUrl);
+
+    beforeAll(() => npmInstall(pkgDir));
+
+    test('resolve @dummy/lib from @dummy/pkg', async () => {
+        const parent = new URL('./package.json', pkgDir).href;
+        const actual = await importMetaResolve('@dummy/lib', parent);
+        const expected = new URL('development.js', libDir).href;
+        expect(actual).toBe(expected);
+    });
+
+    function npmInstall(pkgDir: string | URL) {
+        const exec = promisify(execFile);
+        const npm = process.platform === 'win32' ? 'npm.cmd' : 'npm';
+        return exec(npm, ['install'], { cwd: pkgDir });
+    }
+
 });
