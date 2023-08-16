3c1b64b388db6cb67d4477171cc8643bf85c1a42
nujarum <84607911+nujarum@users.noreply.github.com>
2023-08-16 22:37:04 +0900

    build: update `rollup.config.js`
    - `target`: `node14` => 'node16'

--- rollup.config.js
+++ rollup.config.js
@@ -2,34 +2,52 @@ import { builtinModules } from 'node:module';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import esbuild from 'rollup-plugin-esbuild';
 
-const external = Object.freeze([
-    ...builtinModules,
-    /([/\\])node_modules\1/,
-]);
-
 export const esbuildOptions = Object.freeze({
     format: 'esm',
     minify: true,
     sourcemap: true,
-    target: 'node14',
+    target: 'node16',
     treeShaking: true,
 });
 
-export default {
-    input: 'src/index.ts',
-    external,
-    output: {
-        file: 'dist/index.js',
-        format: 'es',
-        generatedCode: Object.freeze({
-            constBindings: true,
-            objectShorthand: true,
-            symbols: true,
-        }),
-        sourcemap: true,
-    },
-    plugins: [
-        nodeResolve(),
-        esbuild(esbuildOptions),
-    ],
-};
+export const external = Object.freeze([
+    ...builtinModules,
+    /([/\\])node_modules\1/,
+    /^@.+\//,
+    /^#.+/,
+]);
+
+const outputOptions = Object.freeze({
+    format: 'es',
+    generatedCode: Object.freeze({
+        constBindings: true,
+        objectShorthand: true,
+        symbols: true,
+    }),
+    sourcemap: true,
+});
+
+const plugins = Object.freeze([
+    nodeResolve(),
+    esbuild(esbuildOptions),
+]);
+
+const sources = Object.freeze([
+    '_internal',
+    'async/index',
+    'async/worker',
+    'sync/index',
+    'sync/worker',
+]);
+
+export default sources.map(name => {
+    return {
+        input: `./src/${name}.ts`,
+        external,
+        output: {
+            ...outputOptions,
+            file: `./dist/${name}.mjs`,
+        },
+        plugins,
+    };
+});
