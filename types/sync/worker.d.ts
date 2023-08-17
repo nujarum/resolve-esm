de8460bab193a940bd85f2c5fbb88ea1b61b44c2
nujarum <84607911+nujarum@users.noreply.github.com>
2023-08-18 01:18:08 +0900

    feat: update built files

--- types/sync/worker.d.ts
+++ types/sync/worker.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="node" />
 export interface WorkerData {
     readonly buffer: SharedArrayBuffer;
+    readonly names: readonly string[];
     readonly parent?: string | URL | undefined;
-    readonly specifiers: readonly string[];
 }
 //# sourceMappingURL=worker.d.ts.map
